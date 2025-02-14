e4938bc485f6aa1fe04348b51ef1d58e31e82e2d
TMCABC <tmtri22052004@gmail.com>
2025-02-14 21:55:43 +0700

    rebase manage user

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,8 +1,36 @@
-import { Controller, Delete, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Put, Delete, Body, NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UserEntity } from './entities/user.entity';
-
 @Controller('users')
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
+	// 1. Lấy danh sách user
+	@Get()
+	async getAllUsers() {
+		console.log("Received request to get all users");
+		return this.usersService.findAll();
+	}
+
+	@Get(':id')
+	async getUserById(@Param('id') id: string) {
+		console.log(`Fetching user with ID: ${id}`);
+		const user = await this.usersService.findOne(id);
+		if (!user) throw new NotFoundException('User not found');
+		return user;
+	}
+
+	// 2. Khóa/Mở khóa tài khoản
+	@Put(":id/ban")
+	async banUser(@Param("id") id:string){
+		console.log(`Received request to ban user with ID: ${id}`);
+		return this.usersService.banUser(id);
+	}
+	@Put(":id/unban")
+	async unbanUser(@Param("id") id:string){
+		console.log(`Received request to unban user with ID: ${id}`);
+		return this.usersService.unbanUser(id);
+	}
+	@Delete(":id")
+	async deleteUser(@Param("id") id:string){
+		return this.usersService.deleteUser(id);
+	}
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,7 +1,7 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { UserEntity } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
@@ -9,4 +9,42 @@ export class UsersService {
 		@InjectRepository(UserEntity)
 		private readonly userRepository: Repository<UserEntity>,
 	) {}
+	async findOne(id: string): Promise<UserEntity> {
+		return this.userRepository.findOne({ where: { id } });
+	}
+	async findAll(): Promise<UserEntity[]> {
+		return this.userRepository.find();
+	}
+	async toggleUserStatus(id: string, isActive: boolean, action: string): Promise<UserEntity> {
+		const user = await this.userRepository.findOne({ where: { id } });
+		if (!user) throw new NotFoundException('User not found');
+
+		if (user.active === isActive) {
+			throw new BadRequestException(
+				`This account has been ${isActive ? 'active' : 'locked'} before.`
+			);
+		}
+
+		user.active = isActive;
+		await this.userRepository.save(user);
+		console.log(`${user.email}'s account has been ${isActive ? 'unlocked' : 'locked'}.`);
+
+		return user;
+	}
+
+	async banUser(id: string): Promise<UserEntity> {
+		return this.toggleUserStatus(id, false, 'ban');
+	}
+
+	async unbanUser(id: string): Promise<UserEntity> {
+		return this.toggleUserStatus(id, true, 'unban');
+	}
+
+	async deleteUser(id: string): Promise<void> {
+		const user = await this.userRepository.findOne({ where: { id } });
+		if (!user) {
+			throw new NotFoundException('Not found user');
+		}
+		await this.userRepository.remove(user);
+	}
 }
