10c1b6e33d6730e01e085bd42aa0f5d73e7cad3e
datnguyen-boompay <dat@boompay.app>
2025-02-21 10:58:13 +0700

    .

--- src/modules/users/friend.controller.ts
+++ src/modules/users/friend.controller.ts
@@ -1,32 +1,48 @@
-import { Controller, Post, Body, Param, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { FriendService } from './friend.service';
 import { SendFriendRequestDto } from './dto/create-friend-request.dto';
-import { GetUser } from '../../decoractors/ower-or-admin.decorators';
 import { UserEntity } from './entities/user.entity';
-import { JwtAuthGuard } from '../../guards/jwt-auth.guard';
+import { Auth } from '../../decoractors/http.decorators';
+import { RoleType } from '../../constants/role-type';
+import { AuthUser } from '../../decoractors/auth-user.decorators';
 
 @Controller('friends')
-@UseGuards(JwtAuthGuard)
 export class FriendController {
 	constructor(private readonly friendService: FriendService) {}
 
 	@Post('request')
-	async sendFriendRequest(@GetUser() user, @Body() dto: SendFriendRequestDto) {
-		return this.friendService.sendFriendRequest(user.userId, dto.targetId);
+	@Auth([RoleType.USER])
+	async sendFriendRequest(
+		@AuthUser() user: UserEntity,
+		@Body() dto: SendFriendRequestDto,
+	) {
+		return this.friendService.sendFriendRequest(user.id, dto.targetId);
 	}
 
 	@Post('request/:requestId/accept')
-	async acceptFriendRequest(@GetUser() user: UserEntity, @Param('requestId') requestId: string) {
+	@Auth([RoleType.USER])
+	async acceptFriendRequest(
+		@AuthUser() user: UserEntity,
+		@Param('requestId') requestId: string,
+	) {
 		return this.friendService.acceptFriendRequest(requestId);
 	}
 
 	@Post('request/:requestId/reject')
-	async rejectFriendRequest(@GetUser() user: UserEntity, @Param('requestId') requestId: string) {
+	@Auth([RoleType.USER])
+	async rejectFriendRequest(
+		@AuthUser() user: UserEntity,
+		@Param('requestId') requestId: string,
+	) {
 		return this.friendService.rejectFriendRequest(requestId);
 	}
 
 	@Post(':friendId/unrequest')
-	async removeFriendRequest(@GetUser() user, @Param('friendId') friendId: string) {
-		return this.friendService.removeFriendRequest(user.userId,friendId);
+	@Auth([RoleType.USER])
+	async removeFriendRequest(
+		@AuthUser() user: UserEntity,
+		@Param('friendId') friendId: string,
+	) {
+		return this.friendService.removeFriendRequest(user.id, friendId);
 	}
 }
