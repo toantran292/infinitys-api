4690056a39fa4af79b8949d9c1f6ad2b426fd46c
dattamlong <ndat9124@gmail.com>
2025-03-21 07:28:36 +0700

    react

--- src/modules/reacts/reacts.services.ts
+++ src/modules/reacts/reacts.services.ts
@@ -1,15 +1,17 @@
 import { Injectable } from "@nestjs/common";
-import { Repository } from "typeorm";
+import { Repository, DataSource } from "typeorm";
 import { ReactEntity } from "./entities/react.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { UserEntity } from "../users/entities/user.entity";
-import { CreateReactDto } from "./dto/create-react.dto";
+import { CreateReactDto, REACT_TARGET_TYPE } from "./dto/create-react.dto";
+import { PostStatistics } from "../posts/entities/post-statistics.entity";
 
 @Injectable()
 export class ReactsService {
     constructor(
         @InjectRepository(ReactEntity)
         private readonly reactRepository: Repository<ReactEntity>,
+        private readonly dataSource: DataSource,
     ) { }
 
     async findReact(user: UserEntity, data: CreateReactDto) {
@@ -25,20 +27,48 @@ export class ReactsService {
     }
 
     async createReact(user: UserEntity, createReactDto: CreateReactDto) {
-        const existingReact = await this.findReact(user, createReactDto);
+        const queryRunner = this.dataSource.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
 
-        if (existingReact) {
-            existingReact.isActive = !existingReact.isActive;
-            return this.reactRepository.save(existingReact);
+        try {
+            const existingReact = await this.findReact(user, createReactDto);
+            const { targetId, targetType } = createReactDto;
+
+            let savedReact: ReactEntity;
+
+            if (existingReact) {
+                existingReact.isActive = !existingReact.isActive;
+                savedReact = await queryRunner.manager.save(existingReact);
+            } else {
+                const newReact = this.reactRepository.create({
+                    user,
+                    targetId,
+                    targetType,
+                });
+                savedReact = await queryRunner.manager.save(newReact);
+            }
+
+            if (targetType === REACT_TARGET_TYPE.POST) {
+                const delta = savedReact.isActive ? 1 : -1;
+
+                await queryRunner.manager
+                    .createQueryBuilder()
+                    .update(PostStatistics)
+                    .set({
+                        reactCount: () => `react_count + ${delta}`,
+                    })
+                    .where("post_id = :postId", { postId: targetId })
+                    .execute();
+            }
+
+            await queryRunner.commitTransaction();
+            return savedReact;
+        } catch (err) {
+            await queryRunner.rollbackTransaction();
+            throw err;
+        } finally {
+            await queryRunner.release();
         }
-
-        const { targetId, targetType } = createReactDto;
-
-        const newReact = this.reactRepository.create({
-            user,
-            targetId,
-            targetType,
-        });
-        return this.reactRepository.save(newReact);
     }
 }
